d8d3e5887e225e32dc9b2b57c67201b01c65fbd4
Max Ferreira <balt.maxf@gmail.com>
2023-08-15 10:33:05 -0400

    Change

--- GitHubProject/Assets/Scripts/Rotate_script1.cs
+++ GitHubProject/Assets/Scripts/Rotate_script1.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Rotate_script1 : MonoBehaviour
 {
-    private int rot = 360;
+    private float rot = 360;
+    private float rotChange = 1;
+    private float x = 1.01f;
     void Update()
     {
         transform.rotation = Quaternion.Euler(45, rot, 45);
-        rot -= 1;
+        rot -= rotChange;
+        rotChange *= x;
+        if (rotChange >= 5) {x -= 0.02f;}
+        if (rotChange <= 1) {x += 0.02f;}
         if (rot < 0) {rot += 360;}
+        if (rot >= 360) {rot -= 360;}
     }
 }
