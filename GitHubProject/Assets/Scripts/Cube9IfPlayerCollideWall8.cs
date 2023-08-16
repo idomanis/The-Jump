4627f7f2a4c0c06c26539f95202c6b18aabbe742
Max Ferreira <balt.maxf@gmail.com>
2023-08-16 10:19:10 -0400

    Change

--- GitHubProject/Assets/Scripts/Cube9IfPlayerCollideWall8.cs
+++ GitHubProject/Assets/Scripts/Cube9IfPlayerCollideWall8.cs
@@ -1,10 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Cube9IfPlayerCollideWall8 : MonoBehaviour
 {
     [SerializeField] private GameObject Cube9;
-    void Start() => Cube9.transform.position = new Vector3(11.6f, 11.94f, -3.95f);
-    void OnCollisionEnter(Collision other) { if(other.gameObject.tag == "Player") {Cube9.transform.position = new Vector3(11f, 16, -3);}}
+    void Start()
+    {
+        Cube9.transform.position = new Vector3(11.6f, 22.44f, -3.95f);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Player") 
+        {
+            Debug.Log("It is working");
+            Cube9.transform.position = new Vector3(11f, 6, -3);
+        }
+    }
 }
